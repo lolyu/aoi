3a85d883544814415f02871a5dc09f3c29e42927
Longxiang Lyu <lolv@microsoft.com>
2021-11-17 12:23:16 +0000

    rearrange
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

--- scripts/gluster.py
+++ scripts/gluster.py
@@ -1,90 +1,40 @@
-#!/usr/bin/env python
-import os
+# sample code to start gluster service
+# lolyu@redhat.com
+
+from __future__ import print_function
+import docker
 import sys
-import pip
-import imp
-import pexpect
-import subprocess
-import time
-#import pdb
 
-image = "registry.access.redhat.com/rhgs3/rhgs-server-rhel7"
+image_name = "gluster/gluster-centos"
+tag_name = 'latest'
 
-print "****** GlusterFS container bootstrap ******"
-print "[0] check docker installation"
-FNULL = open(os.devnull, 'w')
-try:
-    subprocess.call(['docker'], stdout=FNULL, stderr=FNULL)
-except OSError as e:
-    if e.errno == os.errno.ENOENT:
-        print 'not installed, install then'
-        subprocess.call(['yum', 'install', 'docker', '-y'])
-    else:
-        print str(e)
-        print 'unknown error, exiting...'
+client = docker.from_env()
+image = client.images.pull(image_name, tag=tag_name)
+volumes_dict = {"/etc/glusterfs": {"bind": "/etc/glusterfs", "mode": "z"},
+                "/var/lib/glusterd": {"bind": "/var/lib/glusterd", "mode": "z"},
+                "/var/log/glusterfs": {"bind": "/var/log/glusterfs", "mode": "z"},
+                "/sys/fs/cgroup": {"bind": "/sys/fs/cgroup", "mode": "ro"}}
+#volumes_dict = {"/sys/fs/cgroup": {"bind": "/sys/fs/cgroup", "mode": "ro"}}
+
+container = client.containers.run(image.tags[0], name='gluster', detach=True,
+                                  volumes=volumes_dict, privileged=True)
+
+status, out = container.exec_run('cat /etc/hosts')
+for line in out.splitlines():
+    if container.short_id in line:
+        ip_addr = line.split()[0]
+
+cmd_list = ['systemctl start glusterd',
+            'gluster volume create gv0 %s:/mnt/brick/ force' % ip_addr,
+            'gluster volume start gv0',
+            'gluster volume set gv0 server.allow-insecure on',
+            'gluster volume info gv0']
+
+for cmd in cmd_list:
+    status, out = container.exec_run(cmd)
+    print("cmd: %s" % cmd)
+    print(out)
+    if status != 0:
+        print("Something wrong with %s" % cmd)
+        print("exiting...")
         sys.exit()
-else:
-    print 'docker version'
-    subprocess.call(['docker', 'version'])
-
-print "\n[1] pull the image off"
-out = subprocess.call(['docker', 'pull', image])
-if out != 0:
-    print 'something went wrong pulling images, exiting...'
-    sys.exit()
-
-print
-print "\n[2] create a GlusterFS container"
-#rm existing container named gluster if possible
-if 'gluster' in subprocess.check_output(['docker', 'ps']):
-    subprocess.call(['docker', 'stop', 'gluster'])
-    subprocess.call(['docker', 'rm', 'gluster'])
-
-cmd = 'docker run --privileged=true -it --name gluster %s /bin/bash'
-child = pexpect.spawn(cmd % image)
-child.sendline('/usr/sbin/glusterd')
-
-fmt = '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'
-try:
-    ip = subprocess.check_output(['docker', 'inspect', '-f', fmt, 'gluster'])
-except subprocess.CalledProcessError as e:
-    print str(e)
-    print 'something went wrong with container, exiting...'
-    sys.exit()
-ip = ip.strip()
-
-child.sendline('gluster volume create gv0 %s:/mnt/brick/ force' % ip)
-child.sendline('gluster volume start gv0')
-
-# detach the child process
-# child.interact()
-
-print "\n[3] GlusterFS details"
-fmt = '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'
-print "ip address: %s" % ip
-print "volume: gv0"
-print "container name: gluster"
-
-print "\n[4] check volume connection"
-try:
-    imp.find_module('gluster')
-except ImportError:
-    pip.main(['install', 'gfapi'])
-
-from gluster import gfapi
-volume = gfapi.Volume(host=ip, volname='gv0', log_level=0)
-
-#pdb.set_trace()
-
-time.sleep(5)
-
-try:
-    volume.mount()
-except Exception as e:
-    print str(e)
-    print 'volume seems not exist, exiting...'
-
-if volume.mounted:
-    volume.umount()
-
-print 'server is working well...'
