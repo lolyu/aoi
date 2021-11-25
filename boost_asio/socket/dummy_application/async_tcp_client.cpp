e2113f4b064975d48a2e582da3da43e0589cd9c4
Longxiang Lyu <lolv@microsoft.com>
2021-11-25 09:08:58 +0000

    Async tcp client
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

--- boost_asio/socket/dummy_application/async_tcp_client.cpp
+++ boost_asio/socket/dummy_application/async_tcp_client.cpp
@@ -31,6 +31,8 @@ struct Session
 
     Callback m_callback;
     bool m_was_cancelled;
+    // emulateLongComputationOp is a chain of multiple asynchronous operations, we check the cancel
+    // status between each call to asynchronous operations(obtain the mutex m_cancel_guard)
     std::mutex m_cancel_guard;
 };
 
@@ -39,9 +41,118 @@ class AsyncTCPClient : public boost::noncopyable
 public:
     AsyncTCPClient()
     {
+        // when `io_service::run` is called without a work object, it will return immediately
+        // if it is called with a work object, the event loop will keep running
         m_work.reset(new boost::asio::io_service::work(m_ios));
         m_thread.reset(new std::thread([this]()
                                        { m_ios.run(); }));
+    }
+
+    void emulateLongComputationOp(
+        unsigned int duration_sec,
+        const std::string &raw_ip_address,
+        unsigned short port_num,
+        Callback callback,
+        unsigned int request_id)
+    {
+        std::string request = "EMULATE_LONG_CALC_OP " + std::to_string(duration_sec) + "\n";
+        std::shared_ptr<struct Session> session(new Session(m_ios, raw_ip_address, port_num, request, request_id, callback));
+        session->m_sock.open(session->m_ep.protocol());
+        std::unique_lock<std::mutex> lock(m_active_sessions_guard);
+        m_active_sessions[request_id] = session;
+        lock.unlock();
+
+        session->m_sock.async_connect(session->m_ep, [this, session](const boost::system::error_code &ec)
+                                      {
+                                          if (ec.value() != 0)
+                                          {
+                                              session->m_ec = ec;
+                                              onRequestComplete(session);
+                                              return;
+                                          }
+
+                                          std::unique_lock<std::mutex> cancel_lock(session->m_cancel_guard);
+                                          if (session->m_was_cancelled)
+                                          {
+                                              onRequestComplete(session);
+                                              return;
+                                          }
+
+                                          boost::asio::async_write(session->m_sock,
+                                                                   boost::asio::buffer(session->m_request),
+                                                                   [this, session](const boost::system::error_code &ec, std::size_t bytes_transferred)
+                                                                   {
+                                                                       if (ec.value() != 0)
+                                                                       {
+                                                                           session->m_ec = ec;
+                                                                           onRequestComplete(session);
+                                                                           return;
+                                                                       }
+
+                                                                       std::unique_lock<std::mutex> cancel_lock(session->m_cancel_guard);
+                                                                       if (session->m_was_cancelled)
+                                                                       {
+                                                                           onRequestComplete(session);
+                                                                           return;
+                                                                       }
+
+                                                                       boost::asio::async_read_until(session->m_sock,
+                                                                                                     session->m_response_buf,
+                                                                                                     '\n',
+                                                                                                     [this, session](const boost::system::error_code &ec, std::size_t bytes_transferred)
+                                                                                                     {
+                                                                                                         if (ec.value() != 0)
+                                                                                                         {
+                                                                                                             session->m_ec = ec;
+                                                                                                         }
+                                                                                                         else
+                                                                                                         {
+                                                                                                             std::istream is(&session->m_response_buf);
+                                                                                                             std::getline(is, session->m_response);
+                                                                                                         }
+                                                                                                         onRequestComplete(session);
+                                                                                                     });
+                                                                   });
+                                      });
+    }
+
+    void cancelRequest(unsigned int request_id)
+    {
+        std::unique_lock<std::mutex> lock(m_active_sessions_guard);
+        auto it = m_active_sessions.find(request_id);
+        if (it != m_active_sessions.end())
+        {
+            std::unique_lock<std::mutex> cancel_lock(it->second->m_cancel_guard);
+            it->second->m_was_cancelled = true;
+            it->second->m_sock.cancel();
+        }
+    }
+
+    void close()
+    {
+        // destroy the asio::io_service::work object to allow the I/O event loop thread to exit after all
+        // asynchronous operations are completed
+        m_work.reset(NULL);
+        m_thread->join();
+    }
+
+private:
+    void onRequestComplete(std::shared_ptr<struct Session> session)
+    {
+        std::cout << "Comlete session: " << session->m_id << std::endl;
+        boost::system::error_code ignored_ec;
+        session->m_sock.shutdown(boost::asio::ip::tcp::socket::shutdown_both, ignored_ec);
+        session->m_sock.close();
+        std::unique_lock<std::mutex> lock(m_active_sessions_guard);
+        auto it = m_active_sessions.find(session->m_id);
+        if (it != m_active_sessions.end())
+        {
+            m_active_sessions.erase(it);
+        }
+        lock.unlock();
+
+        boost::system::error_code ec = (session->m_ec.value() == 0 && session->m_was_cancelled) ? boost::asio::error::operation_aborted : session->m_ec;
+        session->m_callback(session->m_id, session->m_response, ec);
     }
 
 private:
@@ -52,7 +163,36 @@ private:
     std::unique_ptr<std::thread> m_thread;
 };
 
+void handler(unsigned int request_id, const std::string &response, const boost::system::error_code &ec)
+{
+    if (ec.value() == 0)
+    {
+        std::cout << "Request #" << request_id << " has completed, response: " << response << std::endl;
+    }
+    else if (ec.value() == boost::asio::error::operation_aborted)
+    {
+        std::cout << "Request #" << request_id << " has been cancelled by the user." << std::endl;
+    }
+    else
+    {
+        std::cout << "Request #" << request_id << " failed! Error code = " << ec.value() << ", error message = " << ec.message() << std::endl;
+    }
+}
+
 int main()
 {
+    try
+    {
+        AsyncTCPClient client;
+        client.emulateLongComputationOp(10, "127.0.0.1", 9000, handler, 1);
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        client.emulateLongComputationOp(11, "127.0.0.1", 9000, handler, 2);
+        client.close();
+    }
+    catch (boost::system::system_error &e)
+    {
+        std::cout << "Error code = " << e.code().value() << ", error message = " << e.what() << std::endl;
+        return e.code().value();
+    }
     return 0;
 }
