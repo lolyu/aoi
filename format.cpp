115a19993dd9b21bb77f7dade7591521ea3fbb42
Longxiang Lyu <lolv@microsoft.com>
2022-02-25 02:19:33 +0000

    Repo level clang format
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

---
+++ format.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+namespace demo
+{
+int i;
+}
+
+void func(
+    int i,
+    int j,
+    int k
+)
+{
+    /* code block */
+}
+
+int main()
+{
+    int i = 10;
+    int j = 10;
+    if (i) {
+        //
+    } else {
+        //
+    }
+    if (
+        i &&
+        j
+    ) {
+        //
+    } else {
+        //
+    }
+    return 0;
+}
