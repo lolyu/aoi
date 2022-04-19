87ff51849639a6dfa204234c99454618135d03ca
Longxiang Lyu <lolv@microsoft.com>
2022-04-19 04:23:18 +0000

    vector operations
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

--- cpp/stl/vector/vector_ops.cpp
+++ cpp/stl/vector/vector_ops.cpp
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,6 +9,9 @@ using namespace std;
 
 void print_double_vector(const vector<double> &vd)
 {
+    // with iterator, we could modify container's content
+    // with const_iterator, it is an iterator that points to a const value(like a `const T *`), dereferencing it returns a reference to a constant value(const T&)
+    // when you have a const reference to the container, you can only get a const_iterator
     for (vector<double>::const_iterator it = vd.begin(); it != vd.end(); ++it) {
         cout << *it << '\t';
     }
@@ -14,6 +20,7 @@ void print_double_vector(const vector<double> &vd)
 
 int main()
 {
+    srand(time(NULL));
     vector<double> vd0(10); // zero initialization
     cout << vd0.size() << endl;
     cout << vd0.capacity() << endl;
@@ -29,5 +36,30 @@ int main()
     vd1.push_back(100.0f);
     vd1.push_back(201.10f);
     print_double_vector(vd1);
+    vd1.swap(vd0);
+    print_double_vector(vd0);
+    print_double_vector(vd1);
+
+    for_each(
+        vd0.begin(), vd0.end(), [](double &d) { d += 10; }
+    );
+    print_double_vector(vd0);
+
+    for_each(
+        vd0.begin(), vd0.end(), [](double &d) { d += rand() % 10; }
+    );
+    random_shuffle(vd0.begin(), vd0.end());
+    print_double_vector(vd0);
+
+    for_each(
+        vd0.begin(), vd0.end(), [](double &d) { d = (d >= 15 ? d : -d); }
+    );
+    print_double_vector(vd0);
+
+    sort(vd0.begin(), vd0.end());
+    print_double_vector(vd0);
+
+    sort(vd0.begin(), vd0.end(), [](const double &l, const double &r) { return abs(l) < abs(r); });
+    print_double_vector(vd0);
     return 0;
 }
