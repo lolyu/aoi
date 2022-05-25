d641c17c53c4585a34d4fdc0b5f0340bdf8db39e
Longxiang Lyu <lolv@microsoft.com>
2022-05-25 09:05:59 +0000

    std::bind passed by value by default
    Signed-off-by: Longxiang Lyu <lolv@microsoft.com>

--- cpp/stl/functor/bind.cpp
+++ cpp/stl/functor/bind.cpp
@@ -17,6 +17,31 @@ struct MyPair {
     double multiply() { return x * y; }
 };
 
+void myfunc(int &n0, int &n1, const int &n2)
+{
+    std::cout << n0 << " " << n1 << " " << n2 << std::endl;
+    ++n0;
+    ++n1;
+}
+
+struct Elem {
+    Elem(int i = 10)
+        : i(i){};
+    Elem(const struct Elem &elem)
+    {
+        std::cout << "Elem copy constructor." << std::endl;
+        this->i = elem.i;
+    }
+
+    int i;
+};
+
+void func_elem_ref(struct Elem &e)
+{}
+
+void func_elem_value(struct Elem e)
+{}
+
 int main()
 {
     auto fn0 = std::bind(my_divide, 10, 2);
@@ -40,6 +65,23 @@ int main()
 
     auto fn5 = std::bind(&MyPair::x, std::placeholders::_1);
     std::cout << fn5(mypair) << std::endl;
+
+    int n0 = 1, n1 = 2, n2 = 3;
+    auto bound_f = std::bind(myfunc, n0, std::ref(n1), std::cref(n2));
+    n0 = 10, n1 = 20, n2 = 30;
+    bound_f();
+    std::cout << n0 << " " << n1 << " " << n2 << std::endl;
+
+    struct Elem elem(100);
+
+    auto bound_func_elem0 = std::bind(func_elem_ref, elem);
+    bound_func_elem0();
+
+    auto bound_func_elem1 = std::bind(func_elem_value, std::ref(elem));
+    bound_func_elem1();
+
+    auto bound_func_elem2 = std::bind(func_elem_ref, std::ref(elem));
+    bound_func_elem2();
 
     return 0;
 }
