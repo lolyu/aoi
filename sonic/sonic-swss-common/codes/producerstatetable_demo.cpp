206995e97bf64f050a14589ec1aadcf5b21420fd
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2022-10-09 13:25:11 +0800

    Update producerstatetable_demo.cpp

--- sonic/sonic-swss-common/codes/producerstatetable_demo.cpp
+++ sonic/sonic-swss-common/codes/producerstatetable_demo.cpp
@@ -1,22 +1,56 @@
-#include <hiredis/hiredis.h>
-
 #include <iostream>
-#include <string>
-#include <vector>
-
-#include "swss/producerstatetable.h"
-#include "swss/rediscommand.h"
-
-using namespace std;
+#include <swss/producerstatetable.h>
 
 int main()
 {
-    swss::DBConnector dbConnector("CONFIG_DB", 0, true);
-    swss::ProducerStateTable producer(&dbConnector, "EMPLOYEE");
-    std::vector<swss::FieldValueTuple> values = {{"name", "alice"}, {"age", "18"}};
-    for (auto &fv : values) {
-        std::cout << fvField(fv) << ": " << fvValue(fv) << std::endl;
+    const std::string dbname = "APPL_DB";
+    const std::string table_name = "PSEUDOTABLE";
+    swss::DBConnector context(dbname, 0, true);
+
+    swss::Table t(&context, table_name);
+    swss::ProducerStateTable pst(&context, table_name);
+
+    std::vector<swss::FieldValueTuple> fvs = {
+        {"key0", "value0"},
+        {"key1", "value1"}};
+    pst.set("ENTRY0", fvs);
+    pst.del("ENTRY0");
+
+    std::vector<swss::KeyOpFieldsValuesTuple> vkco = {
+        {"ENTRY1", "", {{"key0", "value0"}, {"key1", "value1"}}},
+        {"ENTRY2", "", {{"key0", "value0"}, {"key1", "value1"}}},
+    };
+    std::vector<std::string> keys = {"ENTRY1", "ENTRY2"};
+    pst.set(vkco);
+    pst.del(keys);
+
+    pst.set(vkco);
+    pst.clear();
+
+    vkco = {
+        {"ENTRY0", "", {{"key0", "value0"}, {"key1", "value1"}, {"key2", "value2"}}},
+        {"ENTRY1", "", {{"key0", "value0"}, {"key1", "value1"}}},
+        {"ENTRY2", "", {{"key0", "value0"}, {"key1", "value1"}}},
+    };
+    for (auto &kco : vkco)
+    {
+        t.set(kfvKey(kco), kfvFieldsValues(kco));
     }
-    producer.set("ALICE", values);
+
+    pst.create_temp_view();
+    vkco = {
+        {"ENTRY0", "", {{"key0", "value0"}, {"key1", "value11"}, {"key3", "value3"}}},
+        {"ENTRY3", "", {{"key0", "value0"}, {"key1", "value1"}}},
+    };
+    pst.set(vkco);
+    pst.apply_temp_view();
+
+    pst.clear();
+
+    keys.push_back("ENTRY0");
+    for (auto &key : keys)
+    {
+        t.del(key);
+    }
     return 0;
 }
