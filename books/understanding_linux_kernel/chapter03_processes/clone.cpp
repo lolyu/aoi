09420dc9cf00c72be96c330e4d20ccc49c20eeb8
Longxiang Lyu <35479537+lolyu@users.noreply.github.com>
2023-01-05 11:01:51 +0800

    Update clone.cpp

--- books/understanding_linux_kernel/chapter03_processes/clone.cpp
+++ books/understanding_linux_kernel/chapter03_processes/clone.cpp
@@ -7,11 +7,18 @@
 
 using namespace std;
 
+// https://eli.thegreenplace.net/2018/launching-linux-threads-and-processes-with-clone/
+// https://bugzilla.kernel.org/show_bug.cgi?id=203105
+
+volatile pid_t child_tid = 0;
+volatile pid_t parent_tid = 0;
+
 static int child_func(void *arg)
 {
     char *buf = static_cast<char *>(arg);
-    cout << "Child sees buf = \"" << buf << "\"" << endl;
+    printf("Child sees buf = \"%s\"\n", buf);
     strcpy(buf, "hello from child");
+    printf("1Child process pid %d.\n", child_tid);
 }
 
 int main(int argc, char **argv)
@@ -30,13 +37,19 @@ int main(int argc, char **argv)
         flags |= CLONE_VM;
     }
 
+    flags |= CLONE_CHILD_SETTID;
+    flags |= CLONE_PARENT_SETTID;
+
     char buf[100];
     strcpy(buf, "hello from parent");
-    if (clone(child_func, stack + STACK_SIZE, flags | SIGCHLD, buf) == -1)
+    if (clone(child_func, stack + STACK_SIZE, flags | SIGCHLD, buf, &parent_tid, NULL, &child_tid) == -1)
     {
         perror("clone");
         exit(1);
     }
+
+    // cout << "Child pid: " << parent_tid << endl;
+    printf("2Child process pid %d.\n", parent_tid);
 
     int status;
     if (wait(&status) == -1)
@@ -45,7 +58,7 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    cout << "Child exited with status " << status << ". buf = \"" << buf << "\"" << endl;
+    printf("Child exited with status %d. buf = \"%s\"\n", status, buf);
 
     return 0;
 }
